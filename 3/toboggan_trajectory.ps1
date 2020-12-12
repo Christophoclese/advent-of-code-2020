d1eb0c41eafb75452d5dd433926e4da7d5c382b3
Christophoclese <57161+Christophoclese@users.noreply.github.com>
2020-12-12 13:15:30 -0800

    Part 1

---
+++ 3/toboggan_trajectory.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+Param()
+
+# Get input
+[string[]]$Map = @()
+Do {
+    $UserInput = Read-Host -Prompt "Input values (enter blank or 'q' to exit)"
+    If ($UserInput -ne '' -and $UserInput -ne 'q') {
+        $Map += $UserInput
+    }
+} Until ($UserInput -eq '' -or $UserInput -eq 'q')
+
+# Determine the width and height of the input map
+$MapWidth = $Map[0].Length
+$MapHeight = $Map.Count
+
+$X = 0
+$Y = 0
+$TreesHit = 0
+
+# Keep moving right 3, down 1 until we hit the bottom of the input map
+Do {
+    # Move our position
+    $X += 3
+    $Y += 1
+
+    # If our x coordinate is larger than the input map width, we need to wrap back to the other side
+    If ($X -gt $MapWidth - 1) {
+        $X -= $MapWidth
+    }
+
+    # Check for collision with '#' in input map
+    If ($Map[$Y][$X] -eq '#') {
+        Write-Verbose "We hit a tree!"
+        $TreesHit++
+    }
+
+} While ($Y -lt $MapHeight - 1)
+
+Write-Host "Trees hit: $TreesHit"
