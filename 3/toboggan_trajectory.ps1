01c1bae8825d4e36ba3d2cd4ddb34ba1dd8b696b
Christophoclese <57161+Christophoclese@users.noreply.github.com>
2020-12-12 13:21:10 -0800

    Part 2

--- 3/toboggan_trajectory.ps1
+++ 3/toboggan_trajectory.ps1
@@ -10,31 +10,52 @@ Do {
     }
 } Until ($UserInput -eq '' -or $UserInput -eq 'q')
 
-# Determine the width and height of the input map
-$MapWidth = $Map[0].Length
-$MapHeight = $Map.Count
 
-$X = 0
-$Y = 0
-$TreesHit = 0
+function DoSlope {
+    Param(
+        $Map,
 
-# Keep moving right 3, down 1 until we hit the bottom of the input map
-Do {
-    # Move our position
-    $X += 3
-    $Y += 1
+        $Right,
 
-    # If our x coordinate is larger than the input map width, we need to wrap back to the other side
-    If ($X -gt $MapWidth - 1) {
-        $X -= $MapWidth
-    }
+        $Down
+    )
 
-    # Check for collision with '#' in input map
-    If ($Map[$Y][$X] -eq '#') {
-        Write-Verbose "We hit a tree!"
-        $TreesHit++
-    }
+    # Determine the width and height of the input map
+    $MapWidth = $Map[0].Length
+    $MapHeight = $Map.Count
 
-} While ($Y -lt $MapHeight - 1)
+    $X = 0
+    $Y = 0
+    $TreesHit = 0
 
-Write-Host "Trees hit: $TreesHit"
+    # Keep moving right 3, down 1 until we hit the bottom of the input map
+    Do {
+        # Move our position
+        $X += $Right
+        $Y += $Down
+
+        # If our x coordinate is larger than the input map width, we need to wrap back to the other side
+        If ($X -gt $MapWidth - 1) {
+            $X -= $MapWidth
+        }
+
+        # Check for collision with '#' in input map
+        If ($Map[$Y][$X] -eq '#') {
+            Write-Verbose "We hit a tree!"
+            $TreesHit++
+        }
+
+    } While ($Y -lt $MapHeight - 1)
+
+    return $TreesHit
+}
+
+$Answer = 1
+
+$Answer *= DoSlope -Map $Map -Right 1 -Down 1
+$Answer *= DoSlope -Map $Map -Right 3 -Down 1
+$Answer *= DoSlope -Map $Map -Right 5 -Down 1
+$Answer *= DoSlope -Map $Map -Right 7 -Down 1
+$Answer *= DoSlope -Map $Map -Right 1 -Down 2
+
+Write-Host "The magic answer is: $Answer"
